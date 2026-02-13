1b76a75a30cd74ecc4bbe96317235ed9c7079476
Girish N <girishnshobha1524@gmail.com>
2026-02-13 22:22:50 +0530

    Implemented email activation functionality (Part 1) with verification template

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,15 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 
+# verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.conf import settings
+
 
 # Create your views here.
 def register(request):
@@ -23,8 +32,24 @@ def register(request):
             user = Accounts.objects.create_user(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number, password=password, username=username)
             user.phone_number = phone_number
             user.save()
+            
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),      
+            })
+            to_email = email
+            send_mail(mail_subject, message, settings.DEFAULT_FROM_EMAIL, [to_email])
+            
             messages.success(request, 'Registered successfully !')
             return redirect('register')
+            to_email = email
+            send_mail = EmailMessage(mail_subjecct, message, to=[to_email])
+            send_mail.send()
             
     else:
         form = RegistrationForm()
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -140,3 +140,9 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587    
+EMAIL_HOST_USER = 'girish@webenoid.com'
+EMAIL_HOST_PASSWORD = 'giri@1234'
+EMAIL_USE_TLS = True
